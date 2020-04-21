825b8b4ac4e113a9d2c77b2842083ada7a897a7e
tomkf <thomaskeithferris@gmail.com>
2020-04-20 22:30:06 -0700

    remove unnecessary

--- controller/clinicController.js
+++
@@ -1,7 +0,0 @@
-const Clinic = require("../model/ClinicStatus")
-const jwt = require("jsonwebtoken")
-
-const enableCheckinController = async (req, res) => {
-  //
-}
-module.exports.enableCheckinController = enableCheckinController
