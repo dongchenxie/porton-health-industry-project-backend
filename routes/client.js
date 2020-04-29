87c0da216595526667f19ba6e191d088e5c80dab
tomkf <thomaskeithferris@gmail.com>
2020-04-29 14:55:10 -0700

    fix merge conflict

--- routes/client.js
+++ routes/client.js
@@ -2,7 +2,7 @@ const router = require("express").Router()
 const { getAppointmentById } = require("../controller/clientController")
 
 // Get Client/Appointment/{id}
-route.get("/appointment/:appointmentId", async (req, res) => {
+router.get("/appointment/:appointmentId", async (req, res) => {
     return await getAppointmentById(req, res)
 })
 
