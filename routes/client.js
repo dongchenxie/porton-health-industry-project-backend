0ad0b6101fbfa0e06caeba3377b376239a65c39a
Vibha Rana <ranavibha02@gmail.com>
2020-04-29 22:16:01 -0700

    bug fixed

--- routes/client.js
+++ routes/client.js
@@ -1,9 +1,9 @@
 const router = require("express").Router()
 const { getAppointmentById } = require("../controller/clientController")
 
-// Get Client/Appointment/{id}
-route.get("/appointment/:appointmentId", async (req, res) => {
-    return await getAppointmentById(req, res)
-})
+// // Get Client/Appointment/{id}
+ route.get("/appointment/:appointmentId", async (req, res) => {
+     return await getAppointmentById(req, res)
+ })
 
 module.exports = router
