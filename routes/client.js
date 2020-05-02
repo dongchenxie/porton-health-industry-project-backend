6848bd3841527ffe95efa2e1b97fc74f06662a78
BrianH-hub <55164173+BrianH-hub@users.noreply.github.com>
2020-05-01 23:28:31 -0700

    included Auth token verification

--- routes/client.js
+++ routes/client.js
@@ -1,6 +1,6 @@
 const router = require("express").Router()
 const { getAppointmentById, deleteTerminal, getTerminalById, createTerminal } = require("../controller/clientController")
-// const {auth} = require("./verifyToken")
+const {auth} = require("./verifyToken")
 
 
 // Get Client/Appointment/{id}
@@ -9,17 +9,17 @@ router.get("/appointment/:appointmentId", async (req, res) => {
 })
 
 // Get Client/Terminal/{id}
-router.get("/terminal/:terminalId", async (req, res) => {
+router.get("/terminal/:terminalId", auth("CLIENT_ADMIN"), async (req, res) => {
     return await getTerminalById(req, res)
 })
 
 // Delete Client/Terminal/{id}
-router.delete("/terminal/:terminalId", async(req,res)=>{
+router.delete("/terminal/:terminalId", auth("CLIENT_ADMIN"), async (req, res) => {
     return await deleteTerminal(req, res)
 })
 
 // Post Client/Terminal
-router.post("/terminal/:terminalName", async(req,res)=>{
+router.post("/terminal/:terminalName", auth("CLIENT_ADMIN"), async (req, res) => {
     return await createTerminal(req, res)
 })
 
