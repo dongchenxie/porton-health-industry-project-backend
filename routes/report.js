312c35f2725df54a3093d1f5fd8c8564c63bd787
Jinhong Liang <liangjinhongwin@hotmail.com>
2020-05-06 20:23:25 -0700

    Added dummy #67

--- routes/report.js
+++ routes/report.js
@@ -6,7 +6,7 @@ router.get('/appointments', auth('SYSTEM_ADMIN'), async (req, res) => {
     return await getAppointments(req, res)
 })
 
-router.get('/patients', async (req, res) => {
+router.get('/patients', auth('SYSTEM_ADMIN'), async (req, res) => {
     return await getPatients(req, res)
 })
 
